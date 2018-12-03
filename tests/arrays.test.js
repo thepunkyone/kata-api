a95ff1a44ce89167dab65fe46e0ea93e33b65003
Michael Harrison <harrim91@hotmail.co.uk>
2018-12-03 12:18:07 +0000

    Add array tests

---
+++ tests/arrays.test.js
@@ -0,0 +1,87 @@
+describe('/arrays', () => {
+  describe('POST /element-at-index/{index}', () => {
+    xit('returns the element at the given index', (done) => {
+      chai.request(server)
+        .post('/arrays/element-at-index/2')
+        .send({ array: ['cat', 'dog', 'elephant', 'fox'] })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: 'elephant' });
+          done();
+        });
+    });
+  });
+
+  describe('POST /to-string', () => {
+    xit('returns the stringified array', (done) => {
+      chai.request(server)
+        .post('/arrays/to-string')
+        .send({ array: ['cat', 'dog', 'elephant', 'fox'] })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: 'cat,dog,elephant,fox' });
+          done();
+        });
+    });
+  });
+
+  describe('POST /append', () => {
+    xit('returns an array with the value appended', (done) => {
+      chai.request(server)
+        .post('/arrays/append')
+        .send({
+          array: ['cat', 'dog', 'elephant', 'fox'],
+          value: 'gorilla',
+        })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: ['cat', 'dog', 'elephant', 'fox', 'gorilla'] });
+          done();
+        });
+    });
+  });
+
+  describe('POST /starts-with-vowel', () => {
+    xit('returns a filtered array of elements starting with a vowel', (done) => {
+      chai.request(server)
+        .post('/arrays/starts-with-vowel')
+        .send({ array: ['cat', 'dog', 'elephant', 'fox'] })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: ['elephant'] });
+          done();
+        });
+    });
+  });
+
+  describe('POST /remove-element?index={index}', () => {
+    xit('returns an array with the first element removed', (done) => {
+      chai.request(server)
+        .post('/arrays/remove-element')
+        .send({ array: ['cat', 'dog', 'elephant', 'fox'] })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: ['dog', 'elephant', 'fox'] });
+          done();
+        });
+    });
+
+    xit('returns an array with the element at the given index removed', (done) => {
+      chai.request(server)
+        .post('/arrays/remove-element')
+        .send({ array: ['cat', 'dog', 'elephant', 'fox'] })
+        .query({ index: 2 })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: ['cat', 'dog', 'fox'] });
+          done();
+        });
+    });
+  });
+});
