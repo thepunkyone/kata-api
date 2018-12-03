bdab349cdf46c2f6a414bfd1aa4a6cff2bfb44b8
Michael Harrison <harrim91@hotmail.co.uk>
2018-12-03 12:02:26 +0000

    Add boolean tests

---
+++ tests/booleans.test.js
@@ -0,0 +1,159 @@
+describe('/booleans', () => {
+  describe('POST /negate', () => {
+    xit('returns false when passed true', (done) => {
+      chai.request(server)
+        .post('/booleans/negate')
+        .send({ value: true })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: false });
+          done();
+        });
+    });
+
+    xit('returns true when passed false', (done) => {
+      chai.request(server)
+        .post('/booleans/negate')
+        .send({ value: false })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: true });
+          done();
+        });
+    });
+  });
+
+  describe('POST /truthiness', () => {
+    xit('returns false when passed an empty string', (done) => {
+      chai.request(server)
+        .post('/booleans/truthiness')
+        .send({ value: '' })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: false });
+          done();
+        });
+    });
+
+    xit('returns false when passed 0', (done) => {
+      chai.request(server)
+        .post('/booleans/truthiness')
+        .send({ value: 0 })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: false });
+          done();
+        });
+    });
+
+    xit('returns false when passed null', (done) => {
+      chai.request(server)
+        .post('/booleans/truthiness')
+        .send({ value: null })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: false });
+          done();
+        });
+    });
+
+    xit('returns true when passed a string', (done) => {
+      chai.request(server)
+        .post('/booleans/truthiness')
+        .send({ value: 'hello' })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: true });
+          done();
+        });
+    });
+
+    xit('returns true when passed a number', (done) => {
+      chai.request(server)
+        .post('/booleans/truthiness')
+        .send({ value: 9 })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: true });
+          done();
+        });
+    });
+  });
+
+  describe('GET /is-odd/{number}', () => {
+    xit('returns true when passed an odd number', (done) => {
+      chai.request(server)
+        .get('/booleans/is-odd/7')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: true });
+          done();
+        });
+    });
+
+    xit('returns false when passed an even number', (done) => {
+      chai.request(server)
+        .get('/booleans/is-odd/84')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: false });
+          done();
+        });
+    });
+
+    xit('errors when the value is not numeric', (done) => {
+      chai.request(server)
+        .get('/booleans/is-odd/bicycle')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(400);
+          expect(res.body).to.eql({ error: 'Parameter must be a number.' });
+          done();
+        });
+    });
+  });
+
+  describe('GET /{string}/starts-with/{character}', () => {
+    xit('returns true when the string starts with the given character', (done) => {
+      chai.request(server)
+        .get('/booleans/cat/starts-with/c')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: true });
+          done();
+        });
+    });
+
+    xit('returns false when the string does not start with the given character', (done) => {
+      chai.request(server)
+        .get('/booleans/cat/starts-with/d')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: false });
+          done();
+        });
+    });
+
+    xit('errors when the second argument is not a single character', (done) => {
+      chai.request(server)
+        .get('/booleans/cat/starts-with/cat')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(400);
+          expect(res.body).to.eql({ error: 'Parameter "character" must be a single character.' });
+          done();
+        });
+    });
+  });
+});
