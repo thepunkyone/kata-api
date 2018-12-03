dfefb96ed0933edb8193de66293d24e99b980ea0
Michael Harrison <harrim91@hotmail.co.uk>
2018-12-03 11:42:26 +0000

    String challenges

---
+++ tests/strings.test.js
@@ -0,0 +1,65 @@
+describe('/strings', () => {
+  describe('GET /hello/{string}', () => {
+    xit('returns "Hello world!" when passed "world"', (done) => {
+      chai.request(server)
+        .get('/strings/hello/world')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: 'Hello world!' });
+          done();
+        });
+    });
+  });
+
+  describe('GET /upper/{string}', () => {
+    xit('returns the uppercased string', (done) => {
+      chai.request(server)
+        .get('/strings/upper/hello')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: 'HELLO' });
+          done();
+        });
+    });
+  });
+
+  describe('GET /lower/{string}', () => {
+    xit('returns the lowercased string', (done) => {
+      chai.request(server)
+        .get('/strings/lower/HELLO')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: 'hello' });
+          done();
+        });
+    });
+  });
+
+  describe('GET /first-characters/{string}', () => {
+    xit('returns the first character of the string when there is no query string', (done) => {
+      chai.request(server)
+        .get('/strings/first-characters/hello')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: 'h' });
+          done();
+        });
+    });
+
+    xit('returns the first n character of the string when passed a query parameter', (done) => {
+      chai.request(server)
+        .get('/strings/first-characters/sd32fg45')
+        .query({ length: 4 })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.eql({ result: 'sd32' });
+          done();
+        });
+    });
+  });
+});
