0b5599cc8bf8e49d569f7669807a9a318dfe6ada
Vitnija Bluzma <thepunkyone@gmail.com>
2019-05-19 02:17:40 +0100

    refactoring with stringsController

--- src/routes/strings.js
+++ src/routes/strings.js
@@ -1,33 +1,14 @@
 const express = require('express');
-const { sayHello, uppercase, lowercase, firstCharacter, firstCharacters } = require('../lib/strings');
+const stringsController = require('../controllers/strings');
 
 const router = express.Router();
 
-router.get('/hello/:string', (req, res) => {
-  res.status(200).json({ result: sayHello(req.params.string) });
-});
+router.get('/hello/:string', stringsController.hello);
 
-router.get('/upper/:string', (req, res) => {
-  res.status(200).json({ result: uppercase(req.params.string) });
-});
+router.get('/upper/:string', stringsController.uppercase);
 
-router.get('/lower/:string', (req, res) => {
-  res.status(200).json({ result: lowercase(req.params.string) });
-});
+router.get('/lower/:string', stringsController.lowercase);
 
-router.get('/first-characters/:string', (req, res) => {
-  const queryKeys = Object.getOwnPropertyNames(req.query);
-  const numberOfChars = req.query[queryKeys[0]];
-  if (numberOfChars) {
-    res.status(200).json({
-      result: firstCharacters(
-        req.params.string,
-        numberOfChars
-      ),
-    });
-  } else {
-    res.status(200).json({ result: firstCharacter(req.params.string) });
-  }
-});
+router.get('/first-characters/:string', stringsController.firstCharacters);
 
 module.exports = router;
