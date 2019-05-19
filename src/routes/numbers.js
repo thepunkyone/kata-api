202abd1e37fafe6d1da6751ba34914d41c6c7062
Vitnija Bluzma <thepunkyone@gmail.com>
2019-05-19 02:35:05 +0100

    refactoring with numbersController

--- src/routes/numbers.js
+++ src/routes/numbers.js
@@ -1,67 +1,16 @@
 const express = require('express');
-const { add, subtract, multiply, divide, remainder } = require('../lib/numbers');
+const numbersController = require('../controllers/numbers');
 
 const router = express.Router();
 
-router.get('/add/:firstNumber/and/:secondNumber', (req, res) => {
-  const { firstNumber, secondNumber } = req.params;
-  if (isNaN(firstNumber) || isNaN(secondNumber)) {
-    res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: add(Number(firstNumber), Number(secondNumber)) });
-  }
-});
+router.get('/add/:firstNumber/and/:secondNumber', numbersController.add);
 
-router.get('/subtract/:firstNumber/from/:secondNumber', (req, res) => {
-  const { firstNumber, secondNumber } = req.params;
-  if (isNaN(firstNumber) || isNaN(secondNumber)) {
-    res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: subtract(secondNumber, firstNumber) });
-  }
-});
+router.get('/subtract/:firstNumber/from/:secondNumber', numbersController.subtract);
 
-router.post('/multiply', (req, res) => {
-  const { a, b } = req.body;
-  if (a === undefined || b === undefined) {
-    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
-  } else if (isNaN(a) || isNaN(b)) {
-    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: multiply(a, b) });
-  }
-});
+router.post('/multiply', numbersController.multiply);
 
-router.post('/divide', (req, res) => {
-  const { a, b } = req.body;
-  if (a === undefined || b === undefined) {
-    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
-  } else if (typeof a && typeof b === 'number') {
-    if (b === 0) {
-      res.status(400).json({ error: 'Unable to divide by 0.' });
-    }
-    res.status(200).json({ result: divide(a, b) });
-  } else if (isNaN(a) || isNaN(b)) {
-    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: divide(Number(a), Number(b)) });
-  }
-});
+router.post('/divide', numbersController.divide);
 
-router.post('/remainder', (req, res) => {
-  const { a, b } = req.body;
-  if (a === undefined || b === undefined) {
-    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
-  } else if (typeof a && typeof b === 'number') {
-    if (b === 0) {
-      res.status(400).json({ error: 'Unable to divide by 0.' });
-    }
-    res.status(200).json({ result: remainder(a, b) });
-  } else if (isNaN(a) || isNaN(b)) {
-    res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: remainder(Number(a), Number(b)) });
-  }
-});
+router.post('/remainder', numbersController.remainder);
 
 module.exports = router;
